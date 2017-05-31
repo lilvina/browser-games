27d7caf6dfc7497299b72cafe710555d20a522ff
lilvina <davinaleo2007@yahoo.com>
2017-05-31 09:56:56 -0700

    working on chess

---
+++ public/js/chess.js
@@ -0,0 +1,7 @@
+// var board2 = ChessBoard('board2', {
+//   draggable: true,
+//   dropOffBoard: 'trash',
+//   sparePieces: true
+// });
+// $('#startBtn').on('click', board2.start);
+// $('#clearBtn').on('click', board2.clear);
